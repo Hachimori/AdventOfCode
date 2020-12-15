e57bdbeb34a400dfbd4b939de3c17afd9e5d7946
Ben Hachimori <ben.shooter2@gmail.com>
2020-12-15 18:12:10 +0100

    Added 015

---
+++ 2020/015/015-1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+def read():
+    return map(int, raw_input().split(','))
+
+
+def work(vList):
+    v2idx = {}
+    for i in range(len(vList)):
+        v2idx[vList[i]] = i
+    
+    nextVal = 0
+    for idx in range(len(vList), 2019):
+        if nextVal in v2idx:
+            t = v2idx[nextVal]
+            v2idx[nextVal] = idx
+            nextVal = idx - t
+        else:
+            v2idx[nextVal] = idx
+            nextVal = 0
+    
+    print nextVal
+
+
+if __name__ == "__main__":
+    work(read())
