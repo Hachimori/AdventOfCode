09a7fef49d230dd1a4f3e39e1e9d37bf00069082
Ben Hachimori <Ben.shooter2@gmail.com>
2024-12-10 19:54:48 +0900

    Added 010

---
+++ 2024/010/010-1.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int row, col;
+vector<string> ch;
+
+void read() {
+    string line;
+    while (cin >> line) {
+        for (int i = 0; i < line.size(); ++i) {
+            line[i] -= '0';
+        }
+        ch.push_back(line);
+    }
+    row = ch.size();
+    col = ch[0].size();
+}
+
+
+void rec(int r, int c, vector<vector<bool>> &visited, int &ans) {
+    if (ch[r][c] == 9) {
+        ++ans;
+        return;
+    }
+    static int dr[] = {-1, 0, 1, 0};
+    static int dc[] = {0, 1, 0, -1};
+    for (int i = 0; i < 4; ++i) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        if (!(0 <= nr && nr < row && 0 <= nc && nc < col)) {
+            continue;
+        }
+        if (visited[nr][nc]) {
+            continue;
+        }
+        if (ch[nr][nc] != ch[r][c] + 1) {
+            continue;
+        }
+        visited[nr][nc] = true;
+        rec(nr, nc, visited, ans);
+    }
+}
+
+
+void work() {
+    int ans = 0;
+    for (int r = 0; r < row; ++r) {
+        for (int c = 0; c < col; ++c) {
+            if (ch[r][c] != 0) {
+                continue;
+            }
+            vector visited(row, vector(col, false));
+            visited[r][c] = true;
+            rec(r, c, visited, ans);
+        }
+    }
+
+    cout << ans << endl;
+}
+
+
+int main() {
+    read();
+    work();
+    return 0;
+}
